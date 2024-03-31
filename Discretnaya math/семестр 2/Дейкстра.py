45ca0beb267dd3ba233b8ea9da478565360fa1aa
Approbations <120583081+Approbations@users.noreply.github.com>
2024-03-31 12:14:13 +0300

    Update Дейкстра.py

--- Discretnaya math/семестр 2/Дейкстра.py
+++ Discretnaya math/семестр 2/Дейкстра.py
@@ -1,14 +1,21 @@
-edges = list(map(int, input("Введите кол-во вершин, от какой, до какой вершины считать: ").split()))
+edges = list(map(int, input("Введите кол-во вершин, от какой считать минус 1: ").split()))
 vertexes = [list(map(int, input().split())) for _ in range(int(edges[0]))]  # матрица весов
 valid, weights = [True] * edges[0], vertexes[edges[1]].copy()
 weights[edges[1]], valid[edges[1]] = 0, False
+dct_path = dict()
+for i in range(1, edges[0] + 1):
+    dct_path[i] = [i]
 for i in range(edges[0]):
     min_w, number_of_min_w = 10 ** 3 + 1, -1
     for j in range(edges[0]):
         if valid[j] and (weights[j] < min_w):
             min_w, number_of_min_w = weights[j], j
-        for o in range(edges[0]):
-            if weights[number_of_min_w] + vertexes[number_of_min_w][o] < weights[o]:
-                weights[o] = weights[number_of_min_w] + vertexes[number_of_min_w][o]
-                valid[j] = False
-print(f"Минимальный путь от вершины {edges[1]} до вершины {edges[2]}: ", weights[edges[2] - 1])
+    for o in range(edges[0]):
+        if weights[number_of_min_w] + vertexes[number_of_min_w][o] < weights[o]:
+            weights[o] = weights[number_of_min_w] + vertexes[number_of_min_w][o]
+            dct_path[o + 1] = [o + 1]
+            dct_path[o + 1].extend(dct_path[number_of_min_w + 1])
+    valid[number_of_min_w] = False
+print(weights)
+[dct_path[i].append(edges[1] + 1) for i in dct_path.keys() if edges[1] + 1 not in dct_path[i]]
+print(dct_path)
